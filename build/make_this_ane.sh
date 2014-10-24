5302e0bef08d0a337320499cbc015c75ca15f7ca
PeyTy <alegorium@gmail.com>
2014-10-24 07:15:35 +0400

    sh update

--- build/make_this_ane.sh
+++ build/make_this_ane.sh
@@ -35,3 +35,41 @@ ANENAME=$REPLACE
 
 cat extension.xml | sed -e "s/$SEARCH/$REPLACE/g" >> ./temp/extension.xml
 cp platformoptions.xml ./temp/
+
+
+[[ -f "../ane/$ANENAME.ane" ]] && rm -f "../ane/$ANENAME.ane"
+
+SWFVERSION=19
+
+INCLUDE_CLASSES="alegorium.$ANENAME"
+echo "INCLUDE_CLASSES: $INCLUDE_CLASSES"
+
+SRCPATH="../as3/"
+
+# Build:
+echo "GENERATING SWC"
+$ACOMPC -source-path $SRCPATH -include-classes $INCLUDE_CLASSES -swf-version=$SWFVERSION -define+=CONFIG::mock,false -output temp/$ANENAME.swc
+sleep 0
+
+cd temp
+echo "GENERATING LIBRARY from SWC"
+
+unzip $ANENAME.swc
+sleep 0
+[[ -f "catalog.xml" ]] && rm -f "catalog.xml"
+
+echo "GENERATING ANE"
+
+
+
+sleep 0
+
+mv $ANENAME.ane ../../ane/
+
+# Clean:
+[[ -f "library.swf" ]] && rm -f "library.swf"
+[[ -f "$ANENAME.swc" ]] && rm -f "$ANENAME.swc"
+cd ..
+rm -rf temp
+
+echo "DONE!"
