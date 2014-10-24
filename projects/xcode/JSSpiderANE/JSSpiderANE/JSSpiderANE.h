af341daf6579a82d214392cc4badd3d416549465
PeyTy <alegorium@gmail.com>
2014-10-24 07:25:03 +0400

    xcode initial

---
+++ projects/xcode/JSSpiderANE/JSSpiderANE/JSSpiderANE.h
@@ -0,0 +1,13 @@
+//
+//  JSSpiderANE.h
+//  JSSpiderANE
+//
+//  Created by admin on 24.10.14.
+//  Copyright (c) 2014 PeyTy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSSpiderANE : NSObject
+
+@end
