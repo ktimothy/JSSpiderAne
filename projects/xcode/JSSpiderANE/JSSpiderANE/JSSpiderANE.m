722ac4e75a69b798da44eff89ed5c63cf1dcfe78
PeyTy <alegorium@gmail.com>
2014-10-24 07:33:20 +0400

    mock successful build

--- projects/xcode/JSSpiderANE/JSSpiderANE/JSSpiderANE.m
+++ projects/xcode/JSSpiderANE/JSSpiderANE/JSSpiderANE.m
@@ -7,6 +7,7 @@
 //
 
 #import "JSSpiderANE.h"
+#import "FlashRuntimeExtensions.h"
 
 #define DEFINE_ANE_FUNCTION(fn) FREObject (fn)(FREContext context, void* functionData, uint32_t argc, FREObject argv[])
 
@@ -19,3 +20,82 @@
 @implementation JSSpiderANE
 
 @end
+
+// Globalvar
+
+
+DEFINE_ANE_FUNCTION(ADEPEval)
+{
+    // To be filled
+    uint32_t string1Length;
+    const uint8_t *string1;
+    FREGetObjectAsUTF8(argv[0], &string1Length, &string1);
+    
+    // Evaluate script.
+
+    return NULL;
+}
+
+DEFINE_ANE_FUNCTION(ADEPCall)
+{
+    // To be filled
+    uint32_t string1Length;
+    const uint8_t *string1;
+    FREGetObjectAsUTF8(argv[0], &string1Length, &string1);
+    
+    // Evaluate script.
+    
+    
+    // Convert result to string
+    size_t bufferSize;
+    char* buffer;
+    
+    
+    FREObject retVal;
+    FRENewObjectFromUTF8(bufferSize, (const uint8_t*)buffer, &retVal);
+    free(buffer);
+    return retVal;
+}
+
+// A native context instance is created
+void ExtensionContextInitializer(void* extData,
+                                 const uint8_t* ctxType,
+                                 FREContext ctx,
+                                 uint32_t* numFunctionsToSet,
+                                 const FRENamedFunction** functionsToSet){
+    
+    // Initialize the native context.
+    static FRENamedFunction functionMap[] =
+    {
+        MAP_FUNCTION_NAMED((const uint8_t*)"eval", ADEPEval, NULL),
+        MAP_FUNCTION_NAMED((const uint8_t*)"call", ADEPCall, NULL)
+    };
+    
+    *numFunctionsToSet = sizeof( functionMap ) / sizeof( FRENamedFunction );
+	*functionsToSet = functionMap;
+    
+    // Create JavaScript execution context.
+    
+    // Initial script
+   ("window = {};");
+    // Evaluate script.
+   
+}
+
+void ExtensionContextFinalizer(FREContext ctx)
+{
+	// Release JavaScript execution context.
+    
+}
+
+// Initialization function of each extension
+void JSSpiderANEExtensionInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet, FREContextFinalizer* ctxFinalizerToSet) {
+	*extDataToSet = NULL;
+	*ctxInitializerToSet = &ExtensionContextInitializer;
+	*ctxFinalizerToSet = &ExtensionContextFinalizer;
+}
+
+// Called when extension is unloaded
+void JSSpiderANEExtensionFinalizer(void* extData) {
+	return;
+}
