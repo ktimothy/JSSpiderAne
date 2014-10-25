0a8734e2ff37212577b4b26a9ff39ef9a47d4b2f
PeyTy <alegorium@gmail.com>
2014-10-25 21:42:43 +0400

    xcode project backup

---
+++ projects/xcode/JSSpiderANE/JSSpideriOS/JSSpideriOS.m
@@ -0,0 +1,13 @@
+//
+//  JSSpideriOS.m
+//  JSSpideriOS
+//
+//  Created by admin on 24.10.14.
+//  Copyright (c) 2014 PeyTy. All rights reserved.
+//
+
+#import "JSSpideriOS.h"
+
+@implementation JSSpideriOS
+
+@end
