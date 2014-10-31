bb26853db482f82a937dd07826f00bff2991a1f0
PeyTy <alegorium@gmail.com>
2014-11-01 01:40:53 +0400

    android arm build files

---
+++ projects/android/so_arm/jni/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+
+# build ARMv7 version
+include $(CLEAR_VARS)
+include $(LOCAL_PATH)/FlashRuntimeExtensions.mk
+include $(LOCAL_PATH)/libjs_static.mk
+
+LOCAL_MODULE    := libsmonjni
+LOCAL_CFLAGS    := -std=gnu++11 -lm -ldl -static -shared
+LOCAL_LDLIBS    := -llog -lstdc++ -lz
+LOCAL_SRC_FILES := ../../so_cpp/API.cpp
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../so_cpp/include
+
+APP_ABI := armeabi-v7a armeabi
+LOCAL_STATIC_LIBRARIES := libjs_static-prebuilt
+LOCAL_SHARED_LIBRARIES := FlashRuntimeExtensions-prebuilt
+
+include $(BUILD_SHARED_LIBRARY)
