31a62f108b58b32cbbd3005e5e9f7b97b1ba7ee3
PeyTy <alegorium@gmail.com>
2014-11-06 15:49:14 +0400

    builds

--- build/build_sm.sh
+++ build/build_sm.sh
@@ -7,4 +7,10 @@ CD=$DIR
 cd $CD
 #echo "CD: $CD"
 
+../Spidermonkey/js/src/build-ane-osx-x86/build.sh
+../Spidermonkey/js/src/build-ane-ios-armv7/build.sh
+../Spidermonkey/js/src/build-ane-ios-x86/build.sh
+../Spidermonkey/js/src/build-ane-android-armv7/build.sh
+../Spidermonkey/js/src/build-ane-android-x86/build.sh
+
 echo "Done SpiderMonkey!"
