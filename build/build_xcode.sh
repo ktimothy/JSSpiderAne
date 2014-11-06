e0bafcb9d4146a4b74e43fc761b525c6899fabce
PeyTy <alegorium@gmail.com>
2014-11-06 19:08:36 +0400

    Xcode builds

--- build/build_xcode.sh
+++ build/build_xcode.sh
@@ -7,4 +7,20 @@ CD=$DIR
 cd $CD
 #echo "CD: $CD"
 
+cd ../projects/xcode/JSSpiderANE/
+
+rm -rf "./Build/Products/"
+
+xcodebuild -project JSSpiderANE.xcodeproj -target JSSpiderANE
+
+xcodebuild -project JSSpiderANE.xcodeproj -target JSSpideriOS -arch armv7 -sdk iphoneos
+
+xcodebuild -project JSSpiderANE.xcodeproj -target JSSpideriOS -arch i386 -sdk iphonesimulator
+
+mkdir -p "./Build/Products/"
+
+mv "./Build/Release" "./Build/Products/Release"
+mv "./Build/Release-iphoneos" "./Build/Products/"
+mv "./Build/Release-iphonesimulator" "./Build/Products/"
+
 echo "Done xcode!"
