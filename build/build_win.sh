ee6747e876694198432636f0a2585860307cdb45
PeyTy <alegorium@gmail.com>
2014-11-13 21:06:48 +0400

    win32 build script mock

--- build/build_win.sh
+++ build/build_win.sh
@@ -1,2 +1,12 @@
 echo ""
-echo "(win32 build ignored)"
+echo "Building win32"
+
+# Go into current location:
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CD=$DIR
+cd $CD
+#echo "CD: $CD"
+
+
+
+echo "Done win32!"
